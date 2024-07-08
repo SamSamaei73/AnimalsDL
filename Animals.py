45ebf624cf584ed9f79770cb201c2e344d8ca746
samsamaei <alireza.samaei73@gmail.com>
2024-07-08 15:14:31 +0100

    Find Images

--- Animals.py
+++ Animals.py
@@ -3,4 +3,14 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 
-print('sddfsdfsdfsdfsdf')
+DATADIR = './PetImages'
+CATEGORIES = ['Dog', 'Cat']
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img) , cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap='gray')
+        plt.show()
+        break
+    break
